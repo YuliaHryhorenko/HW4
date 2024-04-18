8b267389aa0ec4f4947910de0a24698607d99274
Yuliia Hryhorenko <yuliiahryhorenko@gmail.com>
2024-04-18 12:00:29 +0300

    adding homework

---
+++ js_homework4.js
@@ -0,0 +1,20 @@
+/*    let userAge = prompt(`Скажіть будь ласка, скільки вам років?`);
+      let realAge = Number(userAge);
+
+      if (userAge === null || userAge === "") {
+        alert(`Ви не вказали ваш вік`);
+      } else if (isNaN(realAge) || realAge <= 0) {
+        alert(`Ви вказали невалідний вік`);
+      } else if (realAge % 10 === 1 && realAge % 100 !== 11) {
+        alert(`Вам ${realAge} рік!`);
+      } else if (
+        (realAge % 10 === 2 || realAge % 10 === 3 || realAge % 10 === 4) &&
+        realAge % 100 !== 12 &&
+        realAge % 100 !== 13 &&
+        realAge % 100 !== 14
+      ) {
+        alert(`Вам ${realAge} роки!`);
+      } else {
+        alert(`Вам ${realAge} років!`);
+      }
+*/
